7cc5cf6f35df926b1e8f9a91b8bb4243ebe697db
OLIMPIA\dam104 <marcosbarreiroreiriz@gmail.com>
2017-05-04 10:31:50 +0200

    Correxido

--- Boletin17-Junit/src/Boletin17/Junit/Xeometria.java
+++ Boletin17-Junit/src/Boletin17/Junit/Xeometria.java
@@ -4,48 +4,37 @@ import java.util.Scanner;
 
 public class Xeometria {
 	
-	public void perimetroCadrado(String[] ar) {
-		int lado;
-		System.out.print("Introduce lado del cuadrado: ");
-		Scanner teclado = new Scanner (System.in);
-		lado = teclado.nextInt();
-		System.out.print("Área: " + (lado*lado));
-		teclado.close();
+	public int perímetroCadrado(int lado) {
+		if (lado<0) {
+			return -1;
+		}	
+		return (lado*lado);		
 	}
+
 	
-	public void areaCadrado (String[] ar) {
-		int lado;
-		System.out.print("Introduce lado del cuadrado: ");
-		Scanner teclado = new Scanner (System.in);
-		lado = teclado.nextInt();
-		System.out.print("Perímetro: " + (lado*4));
-		teclado.close();
+	
+	public int areaCadrado(int lado) {
+		if (lado<0) {
+			return -1;
+		}		
+		return (lado*4);
 	}
+
 	
-	public void teoremaPitagoras(String[] ar) {
-		int catetoA,catetoB;
-		System.out.print("Introduce lado A:");
-		Scanner teclado = new Scanner (System.in);
-		catetoA = teclado.nextInt();
-		System.out.print("Introduce lado B:");
-		catetoB = teclado.nextInt();
-		System.out.print("Hipotenusa: " + ((catetoA*catetoA) + (catetoB*catetoB)));
-		teclado.close();
+	public double teoremaPitagoras(int catetoA, int catetoB){
+		if (catetoA<=0 || catetoB <=0) {
+			return -1;
+		}
+		return Math.sqrt(catetoA*catetoA) + (catetoB*catetoB);
 	}
+
 	
-	public void teoremaPitagoras2 (String[] ar) {
-		double catetoA,hipotenusa;
-		System.out.print("Introduce lado A:");
-		Scanner teclado = new Scanner (System.in);
-		catetoA = teclado.nextInt();
-		System.out.print("Introduce hipotenusa:");
-		hipotenusa = teclado.nextInt();
-		System.out.print("LadoB: " + Math.sqrt((catetoA*catetoA) + (hipotenusa*hipotenusa)));
-		teclado.close();
+	
+	public double teoremaPitagoras (double hipotenusa, double catetoA) {
+		if (catetoA<=0 || hipotenusa <=0) {
+			return -1;
+		}
+		return Math.sqrt(catetoA*catetoA) + (hipotenusa*hipotenusa);
 	}
-	
-	
-	
-	
 
 }
