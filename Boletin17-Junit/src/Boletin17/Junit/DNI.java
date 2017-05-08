55b267197c9ab8239963eeae6f1ddc5522075b70
OLIMPIA\dam104 <marcosbarreiroreiriz@gmail.com>
2017-05-08 12:24:45 +0200

    intento DNI

--- Boletin17-Junit/src/Boletin17/Junit/DNI.java
+++ Boletin17-Junit/src/Boletin17/Junit/DNI.java
@@ -1,5 +1,12 @@
 package Boletin17.Junit;
 
 public class DNI {
+	public boolean eValido(string numeroLetra){
+		double numerodni;
+		if (numerodni>=100000000) {
+			return false;
+		} return true ;
+				
+	}
 
 }
