88370f74d9d747f2f363975b8cfe61008cd41ab0
OLIMPIA\dam104 <marcosbarreiroreiriz@gmail.com>
2017-05-15 13:31:59 +0200

    DNI rematado

--- Boletin17-Junit/src/Boletin17/Junit/DNI.java
+++ Boletin17-Junit/src/Boletin17/Junit/DNI.java
@@ -1,12 +1,77 @@
 package Boletin17.Junit;
 
-public class DNI {
-	/*public boolean eValido(string numeroLetra){
-		double numerodni;
-		if (numerodni>=100000000) {
-			return false;
-		} return true ;
-				
-	}*/
+import java.util.ArrayList;
 
+public class DNI {
+
+	
+	public final static char ORDEN_CARACTERES[] = { 'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J',
+			'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E' };
+
+	public boolean eValido(String dni) {
+		if (dni.length() != 9)
+			return false;
+
+		dni = dni.toUpperCase();
+
+		int num = 0, c = 0;
+		for (int i = 0; i < 8; i++) {
+			c = Character.getNumericValue(dni.charAt(i));
+			if (c > 9 || c == -1) break;
+			num += c*Math.pow(10, 7-i);
+		}
+		if (c > 9 || c == -1) return false;
+		
+		char validChar = ORDEN_CARACTERES[num % 23]; 
+
+		if (dni.charAt(8) == validChar) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public boolean eValido(ArrayList<Integer> dni) {
+		if (dni.size() != 9)
+			return false;
+
+		int num = 0;
+		for (int i = 0; i < 8; i++) {
+			num += dni.get(i)*Math.pow(10, 7-i);
+		}
+
+		char validChar = ORDEN_CARACTERES[num % 23]; 
+
+		if (dni.get(8) == validChar || dni.get(8) == Character.toLowerCase(validChar)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public int calculaLetra(String numero_dni) {
+		if (numero_dni.length() != 8)
+			return 0;
+
+		int num = 0, c = 0;
+		for (int i = 0; i < 8; i++) {
+			c = Character.getNumericValue(numero_dni.charAt(i));
+			if (c > 9 || c == -1) break;
+			num += c*Math.pow(10, 7-i);
+		}
+		if (c > 9 || c == -1) return 0;
+		
+		return ORDEN_CARACTERES[num % 23]; 
+	}
+
+	public int calculaLetra(ArrayList<Integer> numero_dni) {
+		if (numero_dni.size() != 8)
+			return 0;
+
+		int num = 0;
+		for (int i = 0; i < 8; i++) {
+			num += numero_dni.get(i)*Math.pow(10, 7-i);
+		}
+		return ORDEN_CARACTERES[num % 23];
+	}
 }
