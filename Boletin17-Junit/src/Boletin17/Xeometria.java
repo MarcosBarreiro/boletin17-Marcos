d9f666f0cffa24208e4ca677482d2076fe73c37e
OLIMPIA\dam104 <marcosbarreiroreiriz@gmail.com>
2017-05-23 12:12:45 +0200

    mods

--- Boletin17-Junit/src/Boletin17/Xeometria.java
+++ Boletin17-Junit/src/Boletin17/Xeometria.java
@@ -6,64 +6,67 @@ package Boletin17;
  *
  */
 public class Xeometria {
-	
-	/**Recibe un lado del cuadrado y devuelve el perimetro
+
+	/**
+	 * Recibe un lado del cuadrado y devuelve el perimetro
 	 * 
 	 * @param lado
 	 * @return perimetro
 	 */
 	public int perimetroCadrado(int lado) {
-		//lado nulo o negativo devuelve error
-		if (lado<=0) {
+		// lado nulo o negativo devuelve error
+		if (lado <= 0) {
 			return -1;
-		}	
-		return (lado*lado);		
+		}
+		return (lado * lado);
 	}
 
-	/**Recibe un lado del cuadrado y devuelve su area
+	/**
+	 * Recibe un lado del cuadrado y devuelve su area
 	 * 
 	 * @param lado
 	 * @return area
 	 */
 	public int areaCadrado(int lado) {
 		// lado nulo o negativo devuelve error
-		if (lado<=0) {
+		if (lado <= 0) {
 			return -1;
-		}		
-		return (lado*4);
+		}
+		return (lado * 4);
 	}
 
-	/**Recibe dos catetos de un triangulo para devolver la hipotenusa
+	/**
+	 * Recibe dos catetos de un triangulo para devolver la hipotenusa
 	 * 
 	 * @param catetoA
 	 * @param catetoB
 	 * @return Hipotenusa
 	 */
-	public double teoremaPitagoras(int catetoA, int catetoB){
-		//catetoA o catetoB nulos o negativos devuelve error
-		if (catetoA<=0 || catetoB <=0) {
+	public double teoremaPitagoras(int catetoA, int catetoB) {
+		// catetoA o catetoB nulos o negativos devuelve error
+		if (catetoA <= 0 || catetoB <= 0) {
 			return -1;
 		}
-		return Math.sqrt(catetoA*catetoA) + (catetoB*catetoB);
+		return Math.sqrt((catetoA * catetoA) + (catetoB * catetoB));
 	}
 
-	
-	/**Recibe la hipotenusa y uno de los catetos y devuelve el
-	 * cateto restante
+	/**
+	 * Recibe la hipotenusa y uno de los catetos y devuelve el cateto restante
 	 * 
 	 * @param hipotenusa
 	 * @param catetoA
 	 * @return catetoB
 	 */
-	public double teoremaPitagoras (double hipotenusa, double catetoA) {
-		//catetoA o hipotenusa nulos o negativos devuelve error
-		if (catetoA<=0 || hipotenusa <=0) {
+	public double teoremaPitagoras(double catetoA, double hipotenusa) {
+		// catetoA o hipotenusa nulos o negativos devuelve error -1
+		if (catetoA <= 0 || hipotenusa <= 0) {
 			return -1;
 		}
+		// CatetoA mayor á hipotenusa devolve error -2
 		if (catetoA > hipotenusa) {
 			return -2;
 		}
-		return Math.sqrt(catetoA*catetoA) + (hipotenusa*hipotenusa);
+		return Math.sqrt((catetoA * catetoA) + (hipotenusa * hipotenusa));
 	}
 
 }
