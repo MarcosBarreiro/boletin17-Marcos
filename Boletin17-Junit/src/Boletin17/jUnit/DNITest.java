af5e879311bf8742a368857a1065abab3feebe3b
Marcos Barreiro <marcosbarreiroreiriz@gmail.com>
2017-05-24 00:40:29 +0200

    Vamo allá ramiro, pero sin copiar eh XD

--- Boletin17-Junit/src/Boletin17/jUnit/DNITest.java
+++ Boletin17-Junit/src/Boletin17/jUnit/DNITest.java
@@ -2,33 +2,47 @@ package Boletin17.jUnit;
 
 import static org.junit.Assert.*;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import Boletin17.DNI;
 
 public class DNITest extends DNI {
-	DNI dni = new DNI();
 	
 	@Test
 	public void testeValidoString() {
-		Assert.assertEquals(dni.eValido("78786658P"), true);
+		DNI dni = new DNI();
+		
+		assertEquals(true, dni.eValido("00000000T"));
+		assertEquals(false, dni.eValido("43567587k"));
+		assertEquals(false, dni.eValido("43567587K"));
+		assertEquals(false, dni.eValido("00000010X"));
+		assertEquals(true, dni.eValido("00000100P"));
+		assertEquals(false, dni.eValido("00000000t"));
+		assertEquals(false, dni.eValido("00000001r"));
+		assertEquals(false, dni.eValido("0000000X"));
 		
 	}
 
 	@Test
 	public void testEValidoArrayListOfInteger() {
-		fail("Not yet implemented");
+	
+		
 	}
 
 	@Test
 	public void testCalculaLetraString() {
-		fail("Not yet implemented");
+		DNI dni = new DNI();
+		
+		assertEquals((int) 'T', dni.calculaLetra("00000000"));
+		assertEquals((int) 'R', dni.calculaLetra("00000001"));
+		assertEquals((int) 'X', dni.calculaLetra("00000010"));
+		assertEquals((int) 'P', dni.calculaLetra("00000100"));
+
 	}
 
 	@Test
 	public void testCalculaLetraArrayListOfInteger() {
-		fail("Not yet implemented");
+	
 	}
 
 }
