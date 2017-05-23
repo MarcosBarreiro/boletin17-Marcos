9640b4245f2c1be4d3c8a6483bd66226e5d60f87
Marcos Barreiro <marcosbarreiroreiriz@gmail.com>
2017-05-24 00:46:42 +0200

    Ahora si que si bro, ya pues copiar!XD

--- Boletin17-Junit/src/Boletin17/jUnit/DNITest.java
+++ Boletin17-Junit/src/Boletin17/jUnit/DNITest.java
@@ -14,8 +14,8 @@ public class DNITest extends DNI {
 		
 		assertEquals(true, dni.eValido("00000000T"));
 		assertEquals(false, dni.eValido("43567587k"));
-		assertEquals(false, dni.eValido("43567587K"));
-		assertEquals(false, dni.eValido("00000010X"));
+		assertEquals(true, dni.eValido("43567587K"));
+		assertEquals(true, dni.eValido("00000010X"));
 		assertEquals(true, dni.eValido("00000100P"));
 		assertEquals(false, dni.eValido("00000000t"));
 		assertEquals(false, dni.eValido("00000001r"));
@@ -35,8 +35,11 @@ public class DNITest extends DNI {
 		
 		assertEquals((int) 'T', dni.calculaLetra("00000000"));
 		assertEquals((int) 'R', dni.calculaLetra("00000001"));
+		assertEquals((int) 'K', dni.calculaLetra("43567587"));
 		assertEquals((int) 'X', dni.calculaLetra("00000010"));
+		assertEquals((int) 'T', dni.calculaLetra("23456435"));
 		assertEquals((int) 'P', dni.calculaLetra("00000100"));
+		assertEquals((int) 'W', dni.calculaLetra("76857699"));
 
 	}
 
